8294a17270de815bd4e7ac98bebeedfde57b0363
yymin1022 <yymin1022@gmail.com>
2023-03-15 21:51:35 +0900

    Update ProjectRouter : Refactor

--- src/Routes/ProjectRouter.ts
+++ src/Routes/ProjectRouter.ts
@@ -5,12 +5,15 @@ import { getProjectData, getProjectList } from "../Utils/FirebaseUtil";
 const projectRouter = express.Router();
 
 projectRouter.post("/getProjectData", async (req: Request, res: Response) => {
-    const API_RESULT = await getProjectData("0001");
+    const PROJECT_ID: string = req.body.PROJECT_ID;
+    const API_RESULT = await getProjectData(PROJECT_ID);
+
     res.send(API_RESULT);
 });
 
 projectRouter.post("/getProjectList", async (req: Request, res: Response) => {
     const API_RESULT = await getProjectList();
+
     res.send(API_RESULT);
 });
 
