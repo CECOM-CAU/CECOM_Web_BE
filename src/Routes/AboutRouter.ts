3bd4eefc6b1127aa8e9ae5601e5ea71c3bd949d0
yymin1022 <yymin1022@gmail.com>
2023-03-15 22:13:21 +0900

    Updatae AboutRouter : Refactor

--- src/Routes/AboutRouter.ts
+++ src/Routes/AboutRouter.ts
@@ -5,16 +5,16 @@ import { getMemberData, getMemberList } from "../Utils/FirebaseUtil";
 const aboutRouter = express.Router();
 
 aboutRouter.post("/getMemberData", async (req: Request, res: Response) => {
-  const ADMIN_TYPE: string = req.body.ADMIN_TYPE;
-  const API_RESULT = await getMemberData(ADMIN_TYPE);
+    const ADMIN_TYPE: string = req.body.ADMIN_TYPE;
+    const API_RESULT = await getMemberData(ADMIN_TYPE);
 
-  res.send(API_RESULT);
+    res.send(API_RESULT);
 });
 
 aboutRouter.post("/getMemberList", async (req: Request, res: Response) => {
-  const API_RESULT = await getMemberList();
+    const API_RESULT = await getMemberList();
 
-  res.send(API_RESULT);
+    res.send(API_RESULT);
 });
 
 export default aboutRouter;
