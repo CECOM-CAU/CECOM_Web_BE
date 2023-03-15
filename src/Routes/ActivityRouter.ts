b66a6d1f30354297a64bc740d5050d8aa7c98567
yymin1022 <yymin1022@gmail.com>
2023-03-15 21:49:18 +0900

    Update ActivityRouter : Removed Unused GET HTTP Function

--- src/Routes/ActivityRouter.ts
+++ src/Routes/ActivityRouter.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from "express";
+
 import { getActivityData, getActivityList } from "../Utils/FirebaseUtil";
 
 const activityRouter = express.Router();
-
-activityRouter.get("/", (req: Request, res: Response) => {
-    res.send("About Router");
-});
 
 activityRouter.post("/getActivityData", async (req: Request, res: Response) => {
     const ACTIVITY_ID: string = req.body.ACTIVITY_ID;
