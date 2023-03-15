14f94c784b257d3762e1ea766c88940d3b4c507e
yymin1022 <yymin1022@gmail.com>
2023-03-15 21:49:02 +0900

    Update ActivityRouter : Removed Unnecessary Comment

--- src/Routes/ActivityRouter.ts
+++ src/Routes/ActivityRouter.ts
@@ -7,19 +7,16 @@ activityRouter.get("/", (req: Request, res: Response) => {
     res.send("About Router");
 });
 
-//특정 활동의 정보를 담은 값 반환
-//매개변수 어떻게 받음?
 activityRouter.post("/getActivityData", async (req: Request, res: Response) => {
     const ACTIVITY_ID: string = req.body.ACTIVITY_ID;
     const API_RESULT = await getActivityData(ACTIVITY_ID);
 
     res.send(API_RESULT);
 });
-  
-  //각 운영진의 역할 및 인원을 담은 List 반환
+
 activityRouter.post("/getActivityList", async (req: Request, res: Response) => {
     const API_RESULT = await getActivityList();
-    
+
     res.send(API_RESULT);
 });
 
