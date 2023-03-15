3190f3c8357c2c9a0e36b674af4516ef745b68e2
yymin1022 <yymin1022@gmail.com>
2023-03-15 21:45:32 +0900

    Update AdminRouter : Removed Unnecessary Comment

--- src/Routes/AboutRouter.ts
+++ src/Routes/AboutRouter.ts
@@ -8,8 +8,6 @@ aboutRouter.get("/", async (req: Request, res: Response) => {
   res.send("this is About Router");
 });
 
-//특정 운영진의 역할 및 인원을 담은 값 반환
-//매개변수 어떻게 받음?
 aboutRouter.post("/getMemberData", async (req: Request, res: Response) => {
   const adminType: string = req.body.AdminType;
   const memberData = await getMemberData(adminType);
@@ -17,7 +15,6 @@ aboutRouter.post("/getMemberData", async (req: Request, res: Response) => {
   res.send(memberData);
 });
 
-//각 운영진의 역할 및 인원을 담은 List 반환
 aboutRouter.post("/getMemberList", async (req: Request, res: Response) => {
   const memberList = await getMemberList();
 
