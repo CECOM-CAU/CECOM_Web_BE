0cdd33b81f172bbc576b7dc579280aa6754501f3
yymin1022 <yymin1022@gmail.com>
2023-03-15 21:45:19 +0900

    Update AdminRouter : Refactor

--- src/Routes/AboutRouter.ts
+++ src/Routes/AboutRouter.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+
 import { getMemberData, getMemberList } from "../Utils/FirebaseUtil";
 
 const aboutRouter = express.Router();
@@ -10,14 +11,17 @@ aboutRouter.get("/", async (req: Request, res: Response) => {
 //특정 운영진의 역할 및 인원을 담은 값 반환
 //매개변수 어떻게 받음?
 aboutRouter.post("/getMemberData", async (req: Request, res: Response) => {
-  const member_data = await getMemberData("Admin");
-  res.send(member_data);
+  const adminType: string = req.body.AdminType;
+  const memberData = await getMemberData(adminType);
+
+  res.send(memberData);
 });
 
 //각 운영진의 역할 및 인원을 담은 List 반환
 aboutRouter.post("/getMemberList", async (req: Request, res: Response) => {
-  const member_list = await getMemberList();
-  res.send(member_list);
+  const memberList = await getMemberList();
+
+  res.send(memberList);
 });
 
 export default aboutRouter;
